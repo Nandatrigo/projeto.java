0d87673e7c334f7801b9963d0319afdd51f8aea9
Fernanda Trigo <nandatrig@yahoo.com>
2024-09-26 21:06:00 -0300

    Entrada do nome do usúario

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Ola, Mundo!");
+        String nome = sc.nextLine();
+
+        sc.close();
+
+        System.out.printf("Olá, %s!", nome);
+        
     }
 }
